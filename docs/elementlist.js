698debc4121eb27abe647e54085fdfc834a02a5c
bimal <gist@bimal.org.np>
2017-12-20 01:46:09 -0700

    Added docs/

---
+++ docs/elementlist.js
@@ -0,0 +1,2 @@
+var ApiGen = ApiGen || {};
+ApiGen.elements = [{"file":"namespace-images.html","label":"images"},{"file":"class-images.cropnail.html","label":"images\\cropnail"},{"file":"class-images.cropper_quick.html","label":"images\\cropper_quick"},{"file":"class-images.cropper.html","label":"images\\cropper"}];
