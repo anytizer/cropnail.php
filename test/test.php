1fa2a8327f6dd175c3d76a492cd30bcd9d896085
server <server@example.com>
2018-03-03 12:10:17 -0700

    file name

--- test/test.php
+++ test/test.php
@@ -3,8 +3,8 @@ namespace test;
 
 require_once "vendor/autoload.php";
 
-#define("__ROOT__", dirname(__FILE__));
-#require_once(__ROOT__."/../src/anytizer/images/class.cropnail.inc.php");
+define("__ROOT__", dirname(__FILE__));
+require_once(__ROOT__."/../src/anytizer/images/class.cropnail.inc.php");
 
 use anytizer\images\cropnail;
 
