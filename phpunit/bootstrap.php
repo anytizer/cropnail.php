0d92f30fe9162e2a9acdb1e08c6b887118013db9
anytizer <anytizer@bimal.org.np>
2019-09-09 21:27:02 -0600

    code cleaning

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -6,7 +6,7 @@ namespace tests;
 require_once "vendor/autoload.php";
 
 define("__ROOT__", dirname(__FILE__));
-require_once __ROOT__ . "/../src/anytizer/images/cropnail.php";
+require_once "../src/anytizer/images/cropnail.php";
 
 /**
  * php phpunit-8.2.2.phar --bootstrap phpunit/bootstrap.php --no-configuration tests/ResizingTest phpunit/tests/ResizingTest.php --teamcity
