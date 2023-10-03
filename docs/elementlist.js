e7e1c7aedb91fb9e840b2bccc879985b62f37b76
Bimal Poudel <anytizer@users.noreply.github.com>
2023-10-02 23:15:53 -0600

    Delete docs directory

--- docs/elementlist.js
+++
@@ -1,8 +0,0 @@
-var ApiGen = ApiGen || {};
-ApiGen.elements = [{"file": "namespace-images.html", "label": "images"}, {
-    "file": "class-images.images.html",
-    "label": "images\\images"
-}, {"file": "class-images.cropper_quick.html", "label": "images\\cropper_quick"}, {
-    "file": "class-images.cropper.html",
-    "label": "images\\cropper"
-}];
